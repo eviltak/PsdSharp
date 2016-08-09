ad91fa9d7da255fd17052fbbe5a1b39bdbe33e00
AravSinghal <singhal.arav@gmail.com>
2016-08-09 22:01:24 +0530

    Add console output to layers test

--- tests/PsdSharp.Tests/PsdFileTests.cs
+++ tests/PsdSharp.Tests/PsdFileTests.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace PsdSharp.Tests
@@ -6,12 +8,21 @@ namespace PsdSharp.Tests
     public class PsdFileTests
     {
         private const string PsdRootPath = "PSDs/";
-        
+
+        private readonly ITestOutputHelper output;
+
+        public PsdFileTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [InlineData(PsdRootPath + "LayersAndMasks.psd")]
         [Theory]
         public void LayersTest(string psdFilePath)
         {
             PsdFile psdFile = new PsdFile(psdFilePath);
+
+            output.WriteLine(string.Join("\n", psdFile.Layers.Select(layer => layer.Name)));
         }
     }
 }
